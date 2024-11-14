bc2ffa8745f1711fbc5ee7333ed5042640e6db2d
Emmanuel Akor <akormanuel@gmail.com>
2024-11-14 19:17:29 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+variable "subnet_id_value" {
+    description = "value for the subnet_id"
+}
+
+variable "name_value" {
+    description = "value for the name of the admin user"
+}
+
+variable "bucket_value" {
+    description = "value for the bucket"
+}
+
+variable "dynamo_name" {
+    description = "value for the DynamoDB table"
+}
+
+variable "billing_mode_value" {
+    description = "value for the billing mode"
+}
